7d6db16aa63f61d942d1e2475cb2d826473b84a3
mangepong <43117908+mangepong@users.noreply.github.com>
2020-09-02 14:09:22 +0200

    Updated some styling

--- src/app/kmom/kmom.component.ts
+++ src/app/kmom/kmom.component.ts
@@ -13,10 +13,19 @@ export class KmomComponent implements AfterViewInit {
 
   ngAfterViewInit() {
       var path = this.route.snapshot.routeConfig.path;
-      var kmom01 = "<h4> Me sida för jsramverk </h4> <p> För att starta sidan behöver du först installera alla node moduler med `npm install` sen kan du skriva `ng serve -o` och då kommer en filk att öppnas i en webbläsare. </p>" +
-     "<h4> Installationer av modulerna </h4>" +
-     "<p> I terminalen så skriver du `npm install -g @angular/cli` för att först ladda ner angular cli. För att sen skapa ditt projekt så kör du `ng new my-app`. </p>" +
-     "<p> Nu kommer den att ska ett angular projekt som du kan öppna med det kommando som står i början: `ng serve -o` </p>" +
+      var kmom01 = "<h4> Me sida för jsramverk </h4> <p> För att starta sidan behöver du först installera alla node moduler med <code>npm install</code> sen kan du skriva <code>ng serve -o</code> och då kommer en filk att öppnas i en webbläsare. </p>" +
+     "<h4> Development server </h4>" +
+     "<p> Run <code>ng serve</code> for a dev server. Navigate to http://localhost:4200/. The app will automatically reload if you change any of the source files. </p>" +
+     "<h4> Code scaffolding </h4>" +
+     "<p> Run <code>ng generate component component-name</code> to generate a new component. You can also use <code>ng generate directive|pipe|service|class|guard|interface|enum|module</code>. </p>" +
+     "<h4> Build </h4>" +
+     "<p> Run <code>ng build</code> to build the project. The build artifacts will be stored in the <code>dist/</code> directory. Use the <code>--prod</code> flag for a production build. </p>" +
+     "<h4> Running unit tests </h4>" +
+     "<p> Run <code>ng test</code> to execute the unit tests via Karma. </p>" +
+     "<h4> Running end-to-end tests </h4>" +
+     "<p> Run <code>ng e2e</code> to execute the end-to-end tests via Protractor. </p>" +
+     "<h4> Further help </h4>" +
+     "<p> To get more help on the Angular CLI use <code>ng help</code> or go check out the Angular CLI README. </p>" +
      "<h4> Github Repo </h4>" +
      "<p> https://github.com/mangepong/jsramverk </p>"
 
